492fb3c6545a3ec8941b30ffcd83b4fcbdc2327b
user <praveen@liveroom.xyz>
2021-12-25 01:49:57 +0530

    add default path to redirect user to /api page for any other url

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,11 @@ const app = express();
 const routes = require('./routes');
 app.use('/', routes);
 
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
